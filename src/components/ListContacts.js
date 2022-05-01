76e503c2d8758855e39c7b562c77b8d2bac51478
Andrei Kuleuski <kuljok@gmail.com>
2022-05-01 23:02:16 +0300

    Render name of every contact

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,7 +1,12 @@
 const ListContacts = ({contacts}) => {
-    console.log(contacts);
     return (
-        <ol className="contact-list"></ol>
+        <ol className="contact-list">
+            {
+                contacts.map((contact) => (
+                    <li key={contact.id}>{contact.name}</li>
+                ))
+            }
+        </ol>
     )
 }
 
