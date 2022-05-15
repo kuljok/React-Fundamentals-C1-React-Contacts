455330531f3fbca764df4b245ad78c3b7e888a33
Andrei Kuleuski <kuljok@gmail.com>
2022-05-15 16:46:03 +0300

    Add avatars to the contacts and update the avatarURL

--- src/components/App.js
+++ src/components/App.js
@@ -7,19 +7,19 @@ const App = () => {
       id: "karen",
       name: "Karen Isgrigg",
       handle: "karen_isgrigg",
-      avatarURL: "http://localhost:5001/karen.jpg",
+      avatarURL: "http://localhost:3000/avatars/karen.jpeg",
     },
     {
       id: "richard",
       name: "Richard Kalehoff",
       handle: "richardkalehoff",
-      avatarURL: "http://localhost:5001/richard.jpg",
+      avatarURL: "http://localhost:3000/avatars/richard.jpeg",
     },
     {
       id: "tyler",
       name: "Tyler McGinnis",
       handle: "tylermcginnis",
-      avatarURL: "http://localhost:5001/tyler.jpg",
+      avatarURL: "http://localhost:3000/avatars/tyler.jpeg",
     },
   ];
 
