9969595f4a4648973efe124be5cc2a8f3e57df1a
Andrei Kuleuski <kuljok@gmail.com>
2022-06-05 19:29:30 +0300

    Filter and sort contacts that are searched for.

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -8,9 +8,13 @@ const ListContacts = ({contacts, onDeleteContact}) => {
         setQuery(query.trim());
     };
 
+    const showingContacts =
+        query === "" ? contacts : contacts.filter((c) =>
+            c.name.toLowerCase().includes(query.toLowerCase())
+        );
+
     return (
         <div className="list-contacts">
-            {console.log(query)}
             <div className="list-contacts-top">
                 <input className="search-contacts" type="text"
                        placeholder="Search Contacts"
@@ -20,7 +24,7 @@ const ListContacts = ({contacts, onDeleteContact}) => {
             </div>
             <ol className="contact-list">
                 {
-                    contacts.map((contact) => (
+                    showingContacts.map((contact) => (
                         <li key={contact.id} className="contact-list-item">
                             <div className="contact-avatar" style={{backgroundImage: `url(${contact.avatarURL})`}}></div>
                             <div className="contact-details">
