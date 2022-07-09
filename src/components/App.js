04eae20fe2a90728595a1dead4901cfe257dce15
Andrei Kuleuski <kuljok@gmail.com>
2022-07-09 17:22:13 +0300

    Add functionality to remove contact from database when deleted in app.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import "../css/App.css";
 import ListContacts from "./ListContacts";
+import * as ContactsAPI from "../utils/ContactsAPI";
 
 const App = () => {
   const [contacts, setContacts] = useState([]);
@@ -16,6 +17,8 @@ const App = () => {
     getContacts();
   }, []);
   const removeContact = (contact) => {
+    ContactsAPI.remove(contact);
+
     setContacts(contacts.filter((c) => c.id !== contact.id));
   }
 
